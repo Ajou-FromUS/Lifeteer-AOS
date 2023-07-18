f0ef2b52df84e6669912f57264ef292561039a96
Jong <jm991014@gmail.com>
2023-07-18 20:52:29 +0900

    [Feat] LandingPage 구현 완료
    AndroidManifest 메인 액티비티 시작으로 바꿔놨습니다.

--- app/src/main/java/presentation/landing/FragmentLandingFourth.kt
+++ app/src/main/java/presentation/landing/FragmentLandingFourth.kt
@@ -1,19 +1,27 @@
 package presentation.landing
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.lifeteer_aos.R
+import com.example.lifeteer_aos.databinding.FragmentLandingFourthBinding
+
 class FragmentLandingFourth : Fragment() {
+    private lateinit var binding: FragmentLandingFourthBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        binding = FragmentLandingFourthBinding.inflate(inflater, container, false)
+        binding.btnStartMindset.setOnClickListener {
+            //val intent = Intent(activity, LoginActivity::class.java)
+            //startActivity(intent)
+        }
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_landing_fourth, container, false)
+        return binding.root
     }
 
 }
--- app/src/main/java/presentation/landing/FragmentLandingThird.kt
+++ app/src/main/java/presentation/landing/FragmentLandingThird.kt
@@ -9,10 +9,6 @@ import com.example.lifeteer_aos.R
 
 class FragmentLandingThird : Fragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
